8470ff7b2838ee19f405e15c9374cbdf2e6ffddd
normchell_pc <nashvlad370@gmail.com>
2023-12-24 09:03:25 +0500

    express init

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 var server = app.listen(3000, function () {
-  var port = server.address().port
+    var port = server.address().port
 
-  console.log(`Listening on ${port}`)
+    console.log(`Listening on ${port}`)
 })
